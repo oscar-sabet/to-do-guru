cd70e99795ee64ad3cd6f1c18a078d5ad6774365
Oscar Sabet <osabet021@gmail.com>
2025-03-09 03:57:10 +0000

    add whitenoise static config

--- todo_guru/settings.py
+++ todo_guru/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "todo_guru.urls"
