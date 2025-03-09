b8ab44fc54dcae03d6a67c667b248a348409bc93
Oscar Sabet <osabet021@gmail.com>
2025-03-09 03:47:45 +0000

    remove whitenoise static config

--- todo_guru/settings.py
+++ todo_guru/settings.py
@@ -142,7 +142,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
